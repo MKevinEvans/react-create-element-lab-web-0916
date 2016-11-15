91bf4fe8e7735c409c7bbca645b3dc939ec03541
mkevinevans <mkevinevans@gmail.com>
2016-11-15 15:58:04 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-//Code React element here
+// <div class="me">
+//   <h1>An Awesome Person</h1>
+//   <p>Who is learning React</p>
+ 
+//   <ul class="me__interests">
+//     <li>JavaScript</li>
+//     <li>React</li>
+//     <li>Movies</li>
+//     <li>Ice cream</li>
+//   </ul>
+// </div>
+alert("whoPPP")
+
+
+
+
+
+let meInReact = React.createElement('div', {className: "me", key: "me"},
+		 React.createElement('h1', {}, "An Awesome Person"),
+		 React.createElement('p', {}, "Who is learning React"),
+		 React.createElement('ul', {className: "me__interests"}, 
+		 	React.createElement('li', {}, "JavaScript"),
+			React.createElement('li', {}, "React"),
+			React.createElement('li', {}, "Movies"),
+			React.createElement('li', {}, "Ice cream") 
+			   ))//Code React element here
+
+ReactDOM.render(meInReact, document.getElementById('main'))
